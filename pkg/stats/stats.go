e3683a4e4549df959b76a210cfb67b36e69697f9
munirhuja1357@gmail.com <bit314151>
2021-12-18 00:52:35 +0500

    categories avg

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -24,3 +24,19 @@ func  TotalInCategory(payments []types.Payment, category types.Category) types.M
 	}
 	return sum
 }
+func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
+	sumCategories := map[types.Category]types.Money{}
+	countCategories := map[types.Category]int{}
+	for _, payment := range payments {
+		if payment.Status != types.StatusFail {
+			sumCategories[payment.Category] += payment.Amount
+			countCategories[payment.Category]++			
+		}
+	}
+	avgCategories := map[types.Category]types.Money{}
+
+	for category, sumCategory := range sumCategories {
+		avgCategories[category] = sumCategory / types.Money(countCategories[category])
+	}
+	return avgCategories
+}
